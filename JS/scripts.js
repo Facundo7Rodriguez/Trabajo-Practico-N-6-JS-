fa19b08836ddf043fe588c0284c7b4b48482e73d
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2022-10-06 21:06:06 -0300

    Calculos Matematicos

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,23 @@
 //#region 
-var A= "Hello";
-let B="World!!!";
+var Apellido="Rodriguez";
+let Nombre="Facundo";
 const N=2;
+//#endregion
+
+//#region 
+Suma (N);
+Resta (N);
+Multiplicacion (N);
+Division (N);
+console.log (Array)
+
+/* Forma vieja del ciclo For */
+for (let i=0; i=6; i++){
+    console.log(A+B)
+}
+/* Forma nueva del ciclo for*/
+//#endregion
+
+//#region 
+
 //#endregion
